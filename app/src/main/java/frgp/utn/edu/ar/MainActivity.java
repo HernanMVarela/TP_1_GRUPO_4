7666d0e3566d6c1e40a2439cb71e384b5827e431
Gonzalo <gonzalo.sinnott@gmail.com>
2023-08-15 19:17:30 -0300

    Agregado Activity Ejercicio 2

--- app/src/main/java/frgp/utn/edu/ar/MainActivity.java
+++ app/src/main/java/frgp/utn/edu/ar/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void navegarEjercicio2(android.view.View view) {
+        android.content.Intent intent = new android.content.Intent(this, Ejercicio2.class);
+        startActivity(intent);
+    }
 }
