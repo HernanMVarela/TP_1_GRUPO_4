70a37a8593f902d6975a3b488b638c6d1774e244
Gonzalo Sinnott Segu <gonzalo.sinnott@gmail.com>
2023-08-15 21:09:10 -0300

    Layout Ejercicio 2

--- app/src/main/java/frgp/utn/edu/ar/Ejercicio2.java
+++ app/src/main/java/frgp/utn/edu/ar/Ejercicio2.java
@@ -3,6 +3,7 @@ package frgp.utn.edu.ar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Ejercicio2 extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class Ejercicio2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio2);
+
+        TextView title = (TextView) findViewById(R.id.tvTitle);
+        title.setText(R.string.title_activity_ejercicio2);
     }
 }
