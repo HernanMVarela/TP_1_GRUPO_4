03891796d6bfc57a2f9ca7d0dd0a0ef8c768de1e
Gonzalo Sinnott Segu <gonzalo.sinnott@gmail.com>
2023-08-16 11:39:40 -0300

    Funcionalidad Operaciones

--- app/src/main/java/frgp/utn/edu/ar/Ejercicio2.java
+++ app/src/main/java/frgp/utn/edu/ar/Ejercicio2.java
@@ -6,21 +6,49 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.fathzer.soft.javaluator.DoubleEvaluator;
+
 public class Ejercicio2 extends AppCompatActivity {
 
-    private TextView tvResultado;
+    private TextView res;
+    boolean isResult = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio2);
-
+        res = (TextView)findViewById(R.id.resultado);
         TextView title = (TextView) findViewById(R.id.tvTitle);
         title.setText(R.string.title_activity_ejercicio2);
-        tvResultado = (TextView)findViewById(R.id.resultado);
     }
 
-    public void Limpiar(View view){
-        tvResultado.setText("0");
+    public void onClick(View view) {
+        addValue(view.getTag().toString());
+    }
+
+    //Metodo para agregar los valores de los botones al textView
+    public void addValue(String value) {
+        if (res.getText().toString().equals("0") || isResult) {
+            res.setText("");
+        }
+        isResult = false;
+        res.setText(res.getText().toString()+value);
+    }
+
+    //Metodo para limpiar el textView
+    public void clear(View view) {
+        res.setText("0");
+    }
+
+    //Metodo para realizar las operaciones
+    public void operar(View view) {
+        DoubleEvaluator evaluator = new DoubleEvaluator();
+        try {
+            String resultado = String.valueOf(evaluator.evaluate(res.getText().toString()));
+            res.setText(resultado);
+        } catch (Exception e) {
+            res.setText("ERROR");
+        }
+        isResult = true;
     }
 }
